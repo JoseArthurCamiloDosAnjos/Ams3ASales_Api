d8e4de58371ab6d9a350566232e455d9dea95899
Gabrielbprado <guardavege@gmail.com>
2024-04-19 11:16:51 -0300

    Adicionar arquivos de projeto.

---
+++ AMS3ASales.api/Context/ApplicationDataContext.cs
@@ -0,0 +1,15 @@
+﻿using AMS3ASales.api.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMS3ASales.api.Context
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options): base(options)
+        { 
+        
+        }
+        public DbSet<Category> Categoriy { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
